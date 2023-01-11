eb413fee5ef1b89072fd6c4c9431f8b6c6a14d9e
jaarhday <jaarhday@byui.edu>
2023-01-11 11:08:32 -0700

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> number = new List<int>();
+        int uNum = 0;
+        int max = 0;
+        int Sum = 0;
+        Console.WriteLine("Enter a list of Numbers, type 0 when finished");
+        do{
+            Console.Write("Enter a Number: ");
+            string input = Console.ReadLine();
+            uNum = int.Parse(input);
+            number.Add(uNum);
+            if (uNum > max){
+                max = uNum;
+            }
+            Sum = uNum + Sum;
+        }while (uNum != 0);
+        Console.WriteLine($"The sum is {Sum}");
+        Console.WriteLine($"The average is {Sum/number.Count}");
+        Console.WriteLine($"The maximum is {max}");
     }
 }
