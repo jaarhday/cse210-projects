6683cdd05c324d6b54ac39a79f1d03cf1b9d853b
jaarhday <jaarhday@byui.edu>
2023-01-11 10:49:29 -0700

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string magicNumber = "";
+        int gNum = 0;
+        Console.WriteLine("What is your magic number: ");
+        magicNumber = Console.ReadLine();
+        int number = int.Parse(magicNumber);
+        do{
+            Console.Write("What is your guess: ");
+            string guess = Console.ReadLine();
+            gNum = int.Parse(guess);
+            if (gNum > number){
+                Console.WriteLine("Lower");
+            }
+            else if (gNum < number){
+                Console.WriteLine("Higher");
+            }
+        }while (gNum != number);
+        Console.WriteLine("You guessed it!");
     }
 }
