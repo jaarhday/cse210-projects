506dcffaf5108f8e38c17c42c00ba4f1540509e2
jaarhday <jaarhday@byui.edu>
2023-01-26 10:15:54 -0700

    Initialize

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string userInput = "";
+        while (userInput != "5"){
+            Console.Write("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Save");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Display");
+            userInput = Console.ReadLine();
+            if (userInput == "1"){
+
+            }
+            else if (userInput == "2"){
+
+            }
+            else if (userInput == "3"){
+
+            }
+            else if (userInput == "4"){
+                
+            }
+        }
     }
 }
